b098f3a85be1fac10bf8c376db12fa608929319c
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-06-08 23:44:59 +0400

    fix image

--- resources/js/store/user-store.js
+++ resources/js/store/user-store.js
@@ -39,7 +39,7 @@ export const useUserStore = defineStore('user', {
 
           this.$state.avatar = res.data.user.avatar
         } else if (!noImage) {
-          this.$state.avatar = 'http://127.0.0.1:8000/img/avatars/default.png'
+          this.$state.avatar = '@public/img/avatars/default.png'
         }
     },
 
@@ -55,7 +55,7 @@ export const useUserStore = defineStore('user', {
         if (res.data.user.avatar) {
           this.$state.avatar = res.data.user.avatar
         } else {
-          this.$state.avatar = 'http://127.0.0.1:8000/img/avatars/default.png'
+          this.$state.avatar = '@public/img/avatars/default.png'
         }
     },
 
