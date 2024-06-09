c689c567e55823018e1fda7926f5f1c2b61a4cd3
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-06-09 20:18:31 +0400

    fix mob vers

--- resources/js/components/sections/home/Heart.vue
+++ resources/js/components/sections/home/Heart.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="cursor-pointer" @click="favorite(product.id)" v-if="product.isFavorite != true && userStore.id == null" src="@public/img/home/main/Favorite.svg" alt="">
+    <img class="cursor-pointer" @click="favorite(product.id)" v-if="product.isFavorite === false && userStore.id == null" src="@public/img/home/main/Favorite.svg" alt="">
     <img class="cursor-pointer" @click="favorite(product.id)" v-else src="@public/img/home/main/FavRed.svg" alt="">
 </template>
 
