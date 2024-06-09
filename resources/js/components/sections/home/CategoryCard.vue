6489ef9cea34c62cbb1c67d2203040e49c9b1cdc
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-06-09 12:51:15 +0400

    fix mobile version

--- resources/js/components/sections/home/CategoryCard.vue
+++ resources/js/components/sections/home/CategoryCard.vue
@@ -38,26 +38,26 @@
     <section class="hidden flex-col gap-[14px] px-5 w-full max-[400px]:px-[10px] max-[400px]:flex max-md:flex max-sm:flex">
         <div class="title text-[#4E4C4C] text-[24px]">Категории</div>
         <div class="categories h-[300px] gap-5 flex w-full justify-between flex-wrap">
-            <router-link :to="{path:'/catalog/Смартфоны'}" class="category-card h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
-                <div class="category-image w-full h-[55px] flex justify-center items-center bg-[#F4F4F4]">
+            <router-link :to="{path:'/catalog/Смартфоны'}" class="category-card max-[400px]:w-[168px] max-[400px]:h-[172px] h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
+                <div class="category-image w-full h-[55px] max-[400px]:h-[50px] flex justify-center items-center bg-[#F4F4F4]">
                     <img src="@public/img/home/mobile/SmartPhoneMobile.png" alt="">
                 </div>
                 <div class="category-name text-[14px] font-[Roboto] flex items-center justify-center h-full w-full text-center">Смартфоны</div>
             </router-link>
-            <router-link :to="{path:'/catalog/Ноутбуки'}" class="category-card h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
-                <div class="category-image w-full h-[55px] flex justify-center items-center bg-[#F4F4F4]">
+            <router-link :to="{path:'/catalog/Ноутбуки'}" class="category-card max-[400px]:w-[168px] max-[400px]:h-[172px] h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
+                <div class="category-image w-full h-[55px] max-[400px]:h-[50px] flex justify-center items-center bg-[#F4F4F4]">
                     <img src="@public/img/home/mobile/Laptop.png" alt="">
                 </div>
                 <div class="category-name text-[14px] font-[Roboto] flex items-center justify-center  h-full w-full text-center">Ноутбуки</div>
             </router-link>
-            <router-link :to="{path:'/catalog/Планшеты'}" class="category-card h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
-                <div class="category-image w-full h-[55px] flex justify-center items-center bg-[#F4F4F4]">
+            <router-link :to="{path:'/catalog/Планшеты'}" class="category-card max-[400px]:w-[168px] max-[400px]:h-[172px] h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
+                <div class="category-image w-full h-[55px] max-[400px]:h-[50px] flex justify-center items-center bg-[#F4F4F4]">
                     <img src="@public/img/home/mobile/Pad.png" alt="">
                 </div>
                 <div class="category-name text-[14px] max-h-[40px] font-[Roboto] flex items-center justify-center h-full w-full text-center">Планшеты</div>
             </router-link>
-            <router-link :to="{path:'/catalog/Смарт-часы'}" class="category-card h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
-                <div class="category-image w-full h-[55px] flex justify-center items-center bg-[#F4F4F4]">
+            <router-link :to="{path:'/catalog/Смарт-часы'}" class="category-card max-[400px]:w-[168px] max-[400px]:h-[172px] h-[112px] w-[185px] shadow-category px-[3px] pt-4 border-t border-gray-100 flex flex-col">
+                <div class="category-image w-full h-[55px] max-[400px]:h-[50px] flex justify-center items-center bg-[#F4F4F4]">
                     <img src="@public/img/home/mobile/Smartwatch screen digital device.png" alt="">
                 </div>
                 <div class="category-name text-[14px] font-[Roboto] flex items-center justify-center h-full w-full text-center">Смарт-часы</div>
