fd61c7e2421913908080acf8be4988739f102d84
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-06-10 11:10:50 +0400

    fix mob vers

--- resources/js/components/sections/errors/NotFound.vue
+++ resources/js/components/sections/errors/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex w-screen h-screen justify-center items-center">
         <div class="flex flex-col gap-3">
-            <div class="404 text-[64px]">404 | Не найдено</div>
+            <div class="404 text-[64px] text-[#05DBF2]">404 | Не найдено</div>
             <div @click="history.go(-1)" class="text-[#151528] text-[48px] duration-200 hover:underline hover:underline-[#151528]">
                 Вернуться обратно
             </div>
